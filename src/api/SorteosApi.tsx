8b4f5b950423261f5104286e4bae1427581a07f1
Enrique Becerra <kabeza@gmail.com>
2023-06-13 21:25:10 -0300

    creando context y cargando api de sorteos

---
+++ src/api/SorteosApi.tsx
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import {URL_API} from '@env';
+const baseURL = URL_API;
+const APISorteos = axios.create({baseURL});
+export default APISorteos;
--- src/screens/PantallaInicio.tsx
+++ src/screens/PantallaInicio.tsx
@@ -1,10 +1,38 @@
-import {View, StyleSheet} from 'react-native';
-import React from 'react';
-import {Button} from 'react-native-paper';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import React, {useContext, useEffect} from 'react';
+import {Button, Card, MD3Colors, Text} from 'react-native-paper';
+import {SorteosContext} from '../context/SorteosContext';
 
 const PantallaInicio = () => {
+  const {cantidad, getSorteos, isLoading, sorteos} = useContext(SorteosContext);
+
+  useEffect(() => {
+    console.log('Aca llama');
+    getSorteos();
+  }, []);
+
   return (
     <View style={estilo.contenedor}>
+      <Card mode="contained" theme={{roundness: 4}} style={{marginBottom: 10}}>
+        <Card.Content>
+          <Text variant="titleLarge">Resultados Quini 6</Text>
+          <Text variant="bodyMedium">
+            Listado de los Últimos Sorteos Realizados
+          </Text>
+        </Card.Content>
+      </Card>
+      {isLoading ? (
+        <ActivityIndicator
+          animating={true}
+          color={MD3Colors.primary50}
+          size={20}
+        />
+      ) : (
+        <>
+          <Text>Ya cargaron los resultados</Text>
+          <Text>Cantidad: {cantidad}</Text>
+        </>
+      )}
       <Button
         mode="contained"
         onPress={() => console.log('Pressed')}
@@ -20,7 +48,7 @@ const PantallaInicio = () => {
 const estilo = StyleSheet.create({
   contenedor: {
     flex: 1,
-    alignItems: 'center',
+    padding: 10,
   },
 });
 
