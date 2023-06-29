ee4004bf79a596b639d0baacee74dc5374d0cdc6
Enrique Becerra <kabeza@gmail.com>
2023-06-28 21:22:34 -0300

    ganaste

--- src/components/Nosirve - ControlarBoleta.tsx
+++ src/components/Nosirve - ControlarBoleta.tsx
@@ -1,22 +1,14 @@
 import * as React from 'react';
 import {useEffect, useRef, useState} from 'react';
-import {Keyboard, StyleSheet, View} from 'react-native';
-import {Button, Text, TextInput, MD3Colors, Card} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {Button, Text, TextInput, MD3Colors} from 'react-native-paper';
 import {DatosSorteo} from '../interfaces/RespuestaDetalleSorteo';
 
 interface Props {
   sorteo: DatosSorteo;
 }
 
-interface TipoAciertos {
-  res0: number; // tradicional
-  res1: number; // la segunda
-  res2: number; // revancha
-  res3: number; // siempre sale
-  res4: number; // pozo extra
-}
-
-interface numerosIngresados {
+interface NumerosSorteo {
   n1: number | string;
   n2: number | string;
   n3: number | string;
@@ -25,10 +17,65 @@ interface numerosIngresados {
   n6: number | string;
 }
 
+interface ResultadosFinales {
+  res0: {
+    titulo?: string;
+    controlado?: boolean;
+    numeros?: number[];
+    aciertos?: number;
+  };
+  res1: {
+    titulo?: string;
+    controlado?: boolean;
+    numeros?: number[];
+    aciertos?: number;
+  };
+  res2: {
+    titulo?: string;
+    controlado?: boolean;
+    numeros?: number[];
+    aciertos?: number;
+  };
+  res3: {
+    titulo?: string;
+    controlado?: boolean;
+    numeros?: number[];
+    aciertos?: number;
+  };
+}
+
 const ControlarBoleta = ({sorteo}: Props) => {
+  const [resFinal, setResFinal] = useState<ResultadosFinales>({
+    res0: {
+      aciertos: 0,
+      controlado: false,
+      numeros: sorteo.resultados[0].numeros.split(',').map(Number),
+      titulo: sorteo.resultados[0].titulo,
+    },
+    res1: {
+      aciertos: 0,
+      controlado: false,
+      numeros: sorteo.resultados[1].numeros.split(',').map(Number),
+      titulo: sorteo.resultados[1].titulo,
+    },
+    res2: {
+      aciertos: 0,
+      controlado: false,
+      numeros: sorteo.resultados[2].numeros.split(',').map(Number),
+      titulo: sorteo.resultados[2].titulo,
+    },
+    res3: {
+      aciertos: 0,
+      controlado: false,
+      numeros: sorteo.resultados[3].numeros.split(',').map(Number),
+      titulo: sorteo.resultados[3].titulo,
+    },
+  });
+  // Mapeo los resultados del sorteo en la interfaz
+
   const [todoOK, setTodoOK] = useState(false);
   const [hayError, setHayError] = useState(false);
-  const [numeros, setNumeros] = useState<numerosIngresados>({
+  const [numeros, setNumeros] = useState<NumerosSorteo>({
     n1: '',
     n2: '',
     n3: '',
@@ -36,15 +83,9 @@ const ControlarBoleta = ({sorteo}: Props) => {
     n5: '',
     n6: '',
   });
-  const [aciertos, setAciertos] = useState<TipoAciertos>({
-    res0: 0,
-    res1: 0,
-    res2: 0,
-    res3: 0,
-    res4: 0,
-  });
 
   useEffect(() => {
+    // console.log(`Numeros = ${JSON.stringify(numeros)}`);
     if (numerosValidos()) {
       setTodoOK(true);
       setHayError(false);
@@ -53,11 +94,6 @@ const ControlarBoleta = ({sorteo}: Props) => {
       setHayError(true);
     }
   }, [numeros]);
-
-  useEffect(() => {
-    console.log(`Aciertos nuevo: ${JSON.stringify(aciertos)}`);
-    // console.log(`Arre Aciertos: ${JSON.stringify(arreAciertos)}`);
-  }, [aciertos]);
 
   // Inputs Refs
   const ref_input1 = useRef();
@@ -107,70 +143,46 @@ const ControlarBoleta = ({sorteo}: Props) => {
   const handleChangeText = (numero: number, valor: string) => {
     switch (numero) {
       case 1:
-        setNumeros(prevState => {
-          return {
-            ...prevState,
-            n1:
-              parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45
-                ? valor
-                : '',
-          };
-        });
+        if (parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45) {
+          setNumeros({...numeros, n1: valor});
+        } else {
+          setNumeros({...numeros, n1: ''});
+        }
         break;
       case 2:
-        setNumeros(prevState => {
-          return {
-            ...prevState,
-            n2:
-              parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45
-                ? valor
-                : '',
-          };
-        });
+        if (parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45) {
+          setNumeros({...numeros, n2: valor});
+        } else {
+          setNumeros({...numeros, n2: ''});
+        }
         break;
       case 3:
-        setNumeros(prevState => {
-          return {
-            ...prevState,
-            n3:
-              parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45
-                ? valor
-                : '',
-          };
-        });
+        if (parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45) {
+          setNumeros({...numeros, n3: valor});
+        } else {
+          setNumeros({...numeros, n3: ''});
+        }
         break;
       case 4:
-        setNumeros(prevState => {
-          return {
-            ...prevState,
-            n4:
-              parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45
-                ? valor
-                : '',
-          };
-        });
+        if (parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45) {
+          setNumeros({...numeros, n4: valor});
+        } else {
+          setNumeros({...numeros, n4: ''});
+        }
         break;
       case 5:
-        setNumeros(prevState => {
-          return {
-            ...prevState,
-            n5:
-              parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45
-                ? valor
-                : '',
-          };
-        });
+        if (parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45) {
+          setNumeros({...numeros, n5: valor});
+        } else {
+          setNumeros({...numeros, n5: ''});
+        }
         break;
       case 6:
-        setNumeros(prevState => {
-          return {
-            ...prevState,
-            n6:
-              parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45
-                ? valor
-                : '',
-          };
-        });
+        if (parseInt(valor, 10) >= 0 && parseInt(valor, 10) <= 45) {
+          setNumeros({...numeros, n6: valor});
+        } else {
+          setNumeros({...numeros, n6: ''});
+        }
         break;
       default:
         break;
@@ -189,6 +201,10 @@ const ControlarBoleta = ({sorteo}: Props) => {
       setHayError(false);
     }
 
+    // Ahora verifico todo porque en teoría está todo bien con los
+    // numeros que ingresó el usuario, debo convertirlos, ordenarlos
+    // hacerles zerofill etc.
+
     // 1 ---- zerofill y ordenar
     let temp = [
       parseInt(numeros.n1.toString(), 10),
@@ -199,65 +215,54 @@ const ControlarBoleta = ({sorteo}: Props) => {
       parseInt(numeros.n6.toString(), 10),
     ];
     let tempSort: number[] = temp.sort((n1, n2) => n1 - n2);
+    let aciertos: number[] = [];
 
-    // Ahora controlo los aciertos por cada sorteo
-
-    // Sorteo Tradicional
-    let s1: number[] = tempSort.filter(element =>
-      sorteo.resultados[0].numeros.split(',').map(Number).includes(element),
+    // 2 ---- compruebo con SORTEO TRADICIONAL
+    aciertos = tempSort.filter(element =>
+      resFinal.res0.numeros.includes(element),
     );
-    setAciertos(prevState => {
-      return {
-        ...prevState,
-        res0: s1.length,
-      };
+    setResFinal({
+      ...resFinal,
+      res0: {aciertos: aciertos.length, controlado: true},
     });
+    console.log(`Aciertos: ${aciertos.length} ${JSON.stringify(aciertos)}`);
 
-    // La Segunda
-    let s2: number[] = tempSort.filter(element =>
-      sorteo.resultados[1].numeros.split(',').map(Number).includes(element),
+    // 3 ---- compruebo con LA SEGUNDA
+    aciertos = tempSort.filter(element =>
+      resFinal.res1.numeros.includes(element),
     );
-    setAciertos(prevState => {
-      return {
-        ...prevState,
-        res1: s2.length,
-      };
+    setResFinal({
+      ...resFinal,
+      res1: {aciertos: aciertos.length, controlado: true},
     });
 
-    // Revancha
-    let s3: number[] = tempSort.filter(element =>
-      sorteo.resultados[2].numeros.split(',').map(Number).includes(element),
+    // 4 ---- compruebo con REVANCHA
+    aciertos = tempSort.filter(element =>
+      resFinal.res2.numeros.includes(element),
     );
-    setAciertos(prevState => {
-      return {
-        ...prevState,
-        res2: s3.length,
-      };
+    setResFinal({
+      ...resFinal,
+      res2: {aciertos: aciertos.length, controlado: true},
     });
 
-    // Siempre Sale
-    let s4: number[] = tempSort.filter(element =>
-      sorteo.resultados[3].numeros.split(',').map(Number).includes(element),
+    // 5 ---- compruebo con SIEMPRE SALE
+    aciertos = tempSort.filter(element =>
+      resFinal.res3.numeros.includes(element),
     );
-    setAciertos(prevState => {
-      return {
-        ...prevState,
-        res3: s4.length,
-      };
+    setResFinal({
+      ...resFinal,
+      res3: {aciertos: aciertos.length, controlado: true},
     });
 
-    // Pozo Extra
-    let s5: string[] = [];
-    s5.push(sorteo.resultados[0].numeros);
-    s5.push(sorteo.resultados[1].numeros);
-    s5.push(sorteo.resultados[2].numeros);
-    s5.push(sorteo.resultados[3].numeros);
-    setAciertos(prevState => {
-      return {
-        ...prevState,
-        res4: s5.length,
-      };
-    });
+    /*
+    const intersection = tempSort.filter(element =>
+      resFinal.res0.numeros.includes(element),
+    );
+    */
+
+    console.log(`ResFinal: ${JSON.stringify(resFinal)}`);
+    // let rf: resultadosFinales;
+    // const res1 = str2.split(',').map(Number);
   };
 
   useEffect(() => {
@@ -386,80 +391,17 @@ const ControlarBoleta = ({sorteo}: Props) => {
           mode="contained"
           onPress={() => {
             setNumeros({n1: '', n2: '', n3: '', n4: '', n5: '', n6: ''});
-            setAciertos({res0: 0, res1: 0, res2: 0, res3: 0, res4: 0});
             setHayError(false);
             setTodoOK(false);
-            ref_input1.current.focus();
-            // Keyboard.dismiss();
           }}
           theme={{roundness: 2}}>
           Limpiar
         </Button>
       </View>
-      {todoOK ? (
-        <>
-          {aciertos.res0 >= 4 ||
-          aciertos.res1 >= 4 ||
-          aciertos.res2 >= 4 ||
-          aciertos.res3 >= 4 ||
-          aciertos.res4 === 6 ? (
-            <View>
-              <Card mode='contained' style={{backgroundColor:'#2a9d8f'}}>
-                <Card.Content style={{alignItems:'center'}}>
-                  <Text
-                    variant="headlineMedium"
-                    style={{color: '#fff', marginBottom: 10}}>
-                    ¡GANASTE!
-                  </Text>
-                  {aciertos.res0 >= 4 ? (
-                    <View style={estilo.contenedorGanaste}>
-                      <View style={{flex:2, paddingLeft:5}}><Text variant="titleMedium">{sorteo.resultados[0].titulo}</Text></View>
-                      <View style={{flex:1, alignItems:'center', paddingRight:5}}><Text variant="titleLarge">{aciertos.res0} aciertos</Text></View>
-                    </View>
-                  ) : null}
-                  {aciertos.res1 >= 4 ? (
-                    <View style={estilo.contenedorGanaste}>
-                      <View style={{flex:2, paddingLeft:5}}><Text variant="titleMedium">{sorteo.resultados[1].titulo}</Text></View>
-                      <View style={{flex:1, alignItems:'center', paddingRight:5}}><Text variant="titleLarge">{aciertos.res1} aciertos</Text></View>
-                    </View>
-                  ) : null}
-                  {aciertos.res2 >= 4 ? (
-                    <View style={estilo.contenedorGanaste}>
-                      <View style={{flex:2, paddingLeft:5}}><Text variant="titleMedium">{sorteo.resultados[2].titulo}</Text></View>
-                      <View style={{flex:1, alignItems:'center', paddingRight:5}}><Text variant="titleLarge">{aciertos.res2} aciertos</Text></View>
-                    </View>
-                  ) : null}
-                  {aciertos.res3 >= 4 ? (
-                    <View style={estilo.contenedorGanaste}>
-                      <View style={{flex:2, paddingLeft:5}}><Text variant="titleMedium">{sorteo.resultados[3].titulo}</Text></View>
-                      <View style={{flex:1, alignItems:'center', paddingRight:5}}><Text variant="titleLarge">{aciertos.res3} aciertos</Text></View>
-                    </View>
-                  ) : null}
-                  {aciertos.res4 === 6 ? (
-                    <View style={estilo.contenedorGanaste}>
-                      <View style={{flex:2, paddingLeft:5}}><Text variant="titleMedium">{sorteo.resultados[4].titulo}</Text></View>
-                      <View style={{flex:1, alignItems:'center', paddingRight:5}}><Text variant="titleLarge">6 aciertos</Text></View>
-                    </View>
-                  ) : null}
-                </Card.Content>
-              </Card>
-            </View>
-          ) : null}
-        </>
-      ) : null}
     </View>
   );
 };
 
 export default ControlarBoleta;
 
-const estilo = StyleSheet.create({
-  contenedorGanaste: {
-    width: '100%',
-    backgroundColor: '#fff',
-    padding: 4,
-    flexDirection: 'row',
-    justifyContent:'center',
-    alignItems: 'center',
-  },
-});
+const styles = StyleSheet.create({});
